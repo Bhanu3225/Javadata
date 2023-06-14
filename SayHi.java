976c4d5196be48da587a59fed7488e80394fb82d
Bhanu <neela_prakash@persistent.com>
2023-06-14 08:35:51 +0530

    first commit

---
+++ SayHi.java
@@ -0,0 +1,9 @@
+package Demo;
+
+public class SayHi {
+    public static void main(String[] args)
+    {
+    System.out.println("Say Hai");
+
+}
+}
---
+++ Simple.java
@@ -0,0 +1,19 @@
+package Demo;
+
+public class Simple {
+
+	public static void main(String[] args) {
+		int a,b;
+		a=5; b=7;
+		int c=a+b;
+		System.out.println(c);
+		
+		
+		int d,e;
+		d=8; e=10;
+		int f=d+e;
+		System.out.println(f);
+
+	}
+
+}
